2f65da727e7dfd0b3d4a79e535f540e17b13d440
Abdullah Tariq <78523853+Abdullahtariq11@users.noreply.github.com>
2026-02-10 21:19:08 -0800

    Time: 0 ms (100%), Space: 42.4 MB (13.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        int [] F=new int[n+1];
+        if(n<=1){
+            return n;
+        }
+        F[0]=0;
+        F[1]=1;
+        for(int i=2;i<=n;i++){
+            F[i]=F[i-2]+F[i-1];
+        }
+        return F[n]; 
+    }
+}
